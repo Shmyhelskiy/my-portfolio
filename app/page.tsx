c908f7c027ed291bb7c7f393560bc42ca1fcfa28
Oleksandr Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2025-05-27 16:42:54 +0300

    fisrt commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 items-center">
+
+    </main>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const createNextIntlPlugin = require('next-intl/plugin')
+import type { NextConfig } from "next";
+
+const withNextIntl = createNextIntlPlugin();
+
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withNextIntl(nextConfig);
